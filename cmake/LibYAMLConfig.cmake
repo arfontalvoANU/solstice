d451349db2fb667d9c862180cf2493089cec5089
Vincent Forest <vincent.forest@meso-star.com>
2016-11-23 15:52:11 +0100

    Fix the LibYAML CMake package on GNU/Linux
    The debug version of the LibYAML library is not necessary on GNU/Linux.

--- cmake/LibYAMLConfig.cmake
+++ cmake/LibYAMLConfig.cmake
@@ -25,12 +25,15 @@ unset(LibYAML_LIBRARY CACHE)
 unset(LibYAML_LIBRARY_DEBUG CACHE)
 unset(LibYAML_LIBRARY_RELWITHDEBINFO CACHE)
 unset(LibYAML_LIBRARY_MINSIZEREL CACHE)
-find_library(LibYAML_LIBRARY yaml DOC 
+find_library(LibYAML_LIBRARY yaml DOC
   "Path to the LibYAML library used during release builds."
   PATH_SUFFIXES bin)
-find_library(LibYAML_LIBRARY_DEBUG yaml-dbg DOC 
+find_library(LibYAML_LIBRARY_DEBUG yaml-dbg DOC
   "Path to the LibYAML library used during debug builds."
   PATH_SUFFIXES bin)
+if(NOT LibYAML_LIBRARY_DEBUG)
+  set(LibYAML_LIBRARY_DEBUG ${LibYAML_LIBRARY})
+endif()
 
 # Create the imported library target
 if(CMAKE_HOST_WIN32)
@@ -39,14 +42,21 @@ else(CMAKE_HOST_WIN32)
   set(_property IMPORTED_LOCATION)
 endif(CMAKE_HOST_WIN32)
 add_library(LibYAML SHARED IMPORTED)
-set_target_properties(LibYAML PROPERTIES 
+set_target_properties(LibYAML PROPERTIES
   ${_property} ${LibYAML_LIBRARY_DEBUG}
   ${_property}_DEBUG ${LibYAML_LIBRARY_DEBUG}
   ${_property}_RELEASE ${LibYAML_LIBRARY})
 
 # Check the package
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibYAML DEFAULT_MSG
-  LibYAML_INCLUDE_DIR
-  LibYAML_LIBRARY)
+if(CMAKE_HOST_WIN32)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibYAML DEFAULT_MSG
+    LibYAML_INCLUDE_DIR
+    LibYAML_LIBRARY_DEBUG
+    LibYAML_LIBRARY)
+else()
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibYAML DEFAULT_MSG
+    LibYAML_INCLUDE_DIR
+    LibYAML_LIBRARY)
+endif()
 
